416bec07a50c54a376f93e7a37057e46034f9fcd
Marissa Zhong <mwz2101@columbia.edu>
2018-06-22 16:31:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    
+    var result = "The line is currently: ";
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      result += (i+1) ". " + katzDeliLine[i] + ', ';
+    }
   }
 }
